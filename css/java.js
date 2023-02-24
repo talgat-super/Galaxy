d2af69ea9c2b433efbc83eaa13e69f70f3d6a28b
talgat-super <dalabaevtalgat4@gamil.com>
2023-02-25 01:36:14 +0600

    First commit

---
+++ css/java.js
@@ -0,0 +1,7 @@
+function showInfo(planet) {
+  if (planet === 'uran') {
+    alert('Uranus is the seventh planet from the Sun. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System. Uranus is similar in composition to Neptune, and both have bulk chemical compositions which differ from that of the larger gas giants Jupiter and Saturn.');
+  } else if (planet === 'saturn') {
+    alert('Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius about nine times that of Earth. It has a prominent system of rings, consisting mostly of ice particles, with a smaller amount of rocky debris and dust.');
+  }
+}
